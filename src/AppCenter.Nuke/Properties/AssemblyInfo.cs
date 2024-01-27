b9535a380da9091cd1f756768900e66cdc0a2027
Enner Pérez <ennerperez@gmail.com>
2024-01-26 22:20:41 -0500

    Initial Commit

---
+++ src/AppCenter.Nuke/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppCenter Nuke")]
+[assembly: AssemblyDescription("AppCenter Nuke")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyCompany("Enner Pérez")]
+[assembly: AssemblyProduct("AppCenter Nuke")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
